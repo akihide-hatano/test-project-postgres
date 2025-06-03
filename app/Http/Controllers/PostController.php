f0ca27b98d637ad08c0777ed0e35b808be74cd26
akihide-hatano <a.h.19841202@gmail.com>
2025-06-03 14:43:03 +0900

    validationの設定も行いました。

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,10 +12,13 @@ class PostController extends Controller
     }
 
     public function store( Request $request){
-        $posts = Post::create([
-            'title'=>$request->title,
-            'body'=>$request->body
+        $validated =$request->validate([
+            'title'=>'required|max:20',
+            'body'=>'required|max:400',
         ]);
+
+        $posts = Post::create($validated);
+        
         $request->session()->flash('message','保存しました');
         return back();
     }
--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -15,13 +15,15 @@
             <div class="mt-8">
                 <div class="w-full flex flex-col">
                     <label for="title" class="font-semibold mt-4">件名</label>
-                    <input id="title" type="text" name="title" class="w-auto py-2 border border-gray-300 rounded-md">
+                    <x-input-error :messages="$errors->get('title')" class="mt-2" />
+                    <input id="title" type="text" name="title" class="w-auto py-2 border border-gray-300 rounded-md" value="{{old('title')}}">
                 </div>
             </div>
 
                 <div class="w-full flex flex-col">
                     <label for="body" class="font-semibold mt-4">本文</label>
-                    <textarea name="body" id="body" cols="30" rows="5" class="w-auto py-2 border border-gray-300 rounded-md"></textarea>
+                    <x-input-error :messages="$errors->get('body')" class="mt-2" />
+                    <textarea name="body" id="body" cols="30" rows="5" class="w-auto py-2 border border-gray-300 rounded-md" value="{{old('body')}}"></textarea>
                 </div>
 
                 <x-primary-button class="mt-4">
