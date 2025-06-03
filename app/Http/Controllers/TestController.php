5e1f7b0c7a651bcd66a3dc6041a1a6192c5abc2d
akihide-hatano <a.h.19841202@gmail.com>
2025-06-03 11:04:49 +0900

    capter4-4を終了しました。 2025_06_03_110359_create_posts_tableを作成しました。

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -1,12 +1,13 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class TestController extends Controller
 {
     public function test(){
-        return view('test');
+        $users = User::all();
+        return view('test',compact('users'));
     }
 }
