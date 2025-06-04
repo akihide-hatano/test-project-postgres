a287ea34f88d2ea54fc9839960c72e3cfa9b8adb
akihide-hatano <a.h.19841202@gmail.com>
2025-06-04 09:37:18 +0900

    capter8終了しました。

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,6 +3,9 @@
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
         </h2>
+        @can('test')
+            テストゲートです。
+        @endcan
     </x-slot>
 
     <div class="py-12">
