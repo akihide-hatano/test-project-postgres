fec63c1005d74b7af2c2afdd8e359a0fc5ea6988
akihide-hatano <a.h.19841202@gmail.com>
2025-06-05 14:50:07 +0900

    capter10完了しました。

--- resources/views/components/message.blade.php
+++ resources/views/components/message.blade.php
@@ -1,8 +1,7 @@
-<div>
-    <!-- No surplus words or unnecessary actions. - Marcus Aurelius -->
+<!-- No surplus words or unnecessary actions. - Marcus Aurelius -->
+    @props(['message'])
     @if ($message)
         <div class="p-4 m-2 rounded bg-green-100">
             {{$message}}
         </div>
     @endif
-</div>
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,9 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/dashboard',[PostController::class,'index'])->middleware(['auth'])
+->name('dashboard');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
