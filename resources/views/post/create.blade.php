3ca610857a86cb2a1d74ca2e2a9b7e55d1c2ddcf
akihide-hatano <a.h.19841202@gmail.com>
2025-06-23 11:56:28 +0900

    create.blade.phpにてErrorの表示を修正

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -13,6 +13,7 @@
                     <label for="title" class="font-semibold mt-4">件名</label>
                     <x-input-error :messages="$errors->get('title')" class="mt-2" />
                     <input id="title" type="text" name="title" class="w-auto py-2 border border-gray-300 rounded-md" value="{{old('title')}}">
+                    <p id="titleError" class="text-red-500 text-sm mt-1"></p>
                 </div>
             </div>
 
@@ -20,7 +21,7 @@
                     <label for="body" class="font-semibold mt-4">本文</label>
                     <x-input-error :messages="$errors->get('body')" class="mt-2" />
                     <textarea name="body" id="body" cols="30" rows="5" class="w-auto py-2 border border-gray-300 rounded-md" value="{{old('body')}}"></textarea>
-
+                    <p id="bodyError" class="text-red-500 text-sm mt-1"></p>
                     <div class="text-right text-sm text-gray-500 mt-2">
                         <span id="charCount">0</span>/<span id="maxChars">400</span>文字
                     </div>
