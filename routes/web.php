d2c826518aaa7d5111890de4327d41b07d33407e
akihide-hatano <a.h.19841202@gmail.com>
2025-06-23 17:34:00 +0900

    web.phpでverifyEmailを有効にする

--- routes/web.php
+++ routes/web.php
@@ -10,11 +10,7 @@ Route::get('/', function () {
 
 Route::resource('post',PostController::class);
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::get('/dashboard',[PostController::class,'index'])->middleware(['auth'])
+Route::get('/dashboard',[PostController::class,'index'])->middleware(['auth', 'verified'])
 ->name('dashboard');
 
 Route::middleware('auth')->group(function () {
